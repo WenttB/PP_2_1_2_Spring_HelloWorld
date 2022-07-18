ff554513a868b527957bc3db3470b7813294581a
WenttB <101995345+WenttB@users.noreply.github.com>
2022-07-19 04:10:42 +0500

    init1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,13 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        boolean eq = bean == bean2;
         System.out.println(bean.getMessage());
+        System.out.println(eq);
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        boolean eqCat = beanCat == beanCat1;
+        System.out.println(eqCat);
     }
 }
